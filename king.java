f8bf9571546085e8f4171f0fa81c7748b6974025
Emre Alyamaç <145257554+EmreAlyamac@users.noreply.github.com>
2023-12-22 18:36:16 +0300

    Update king.java

--- king.java
+++ king.java
@@ -13,9 +13,38 @@ class king extends piece{
             this.path = "blackKing.png";
         }
     }
+    /*
+     * Besides the protection requirement, the king is done is wished there could be helper method that checks for all the pieces on the board and looks if a spesific piece can reach the king. if it moved
+     * if return true then the king is in check
+     * if return false then the king is not in check
+     * also we can restrict the king from moving to a position that will put it in check
+     */
+    public Boolean isInCheck(piece[][] board, int x, int y){
+
+    
+        for (int i = 0; i < 8; i++){
+            for(int j = 0; j < 8; j++){
+                piece currentPiece = board[i][j];
+                if (currentPiece != null && currentPiece.getAlive() && currentPiece.getColor() != this.color && currentPiece.getType() != "king"){
+                    if (currentPiece.check_valid(x, y, board)){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
 
     public Boolean check_valid(int x, int y, piece[][] board){
         Boolean move = false;
+
+        if (isInCheck(board, x, y)){
+            return false;
+        }
+
+        if(board[x][y].getAlive() && board[x][y].getColor() == this.color){
+            return false;
+        }
 
         
         if (x == this.x && y == this.y){
@@ -56,6 +85,15 @@ class king extends piece{
         if (board[x][y].getAlive() && board[x][y].getColor() != this.color){
             eat = true;
         }
+
+        if (isInCheck(board, x, y)){
+            return false;
+        }
+
+        if(board[x][y].getAlive() && board[x][y].getColor() == this.color){
+            return false;
+        }
+
         Boolean move = false;
 
         if (x == this.x && y == this.y){
